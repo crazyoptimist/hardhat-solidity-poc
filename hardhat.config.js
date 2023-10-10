40403658eff866cea12d3cfab361f2c695bfcbe4
crazyoptimist <hey@crazyoptimist.net>
2023-10-10 12:08:12 -0500

    feat: deploy to op goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    "optimism-goerli": {
+      url: `https://optimism-goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${process.env.OWNER_PRIVATE_KEY}`],
+    },
+  },
 };
